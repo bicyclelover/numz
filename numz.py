0805e24d08a031456669c5c68866261509a85284
bicyclelover <jamesdavidnicholson@gmail.com>
2022-10-14 13:18:32 -0400

    'adf'

--- numz.py
+++ numz.py
@@ -10,14 +10,16 @@ class numz_game:
         self.pieces = ['r1','b1','r2','b2','r3','b3']
         self.board = [[0,0,0],[0,0,0],[0,0,0]]
         self.move_number = 0
-        self.piece_positions = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+        self.piece_positions = [[4,4] for i in range(6)]
         self.rows = self.board
         self.columns = [[self.board[i][x] for i in [0,1,2]] for x in [0,1,2]]
         self.diagonals = [[self.board[i][i] for i in [0,1,2]],
                           [self.board[i[0]][i[1]]
                            for i in [[2,0], [1,1], [0,2]]]]
         self.red_pieces = ['r1','r2', 'r3']
+        self.red_backwards = ['r3', 'r2', 'r1']
         self.blue_pieces = ['b1', 'b2', 'b3']
+        self.blue_backwards = ['b3', 'b2', 'b1']
                           
         
     def __str__(self):
@@ -38,16 +40,16 @@ class numz_game:
     def check_winning(self):
         #rows
         for i in self.board:
-            if i == self.red_pieces or i ==self.red_pieces.reverse():
+            if i == self.red_pieces or i ==self.red_backwards:
                 return True
-            if i == self.blue_pieces or i == self.blue_pieces.reverse():
+            if i == self.blue_pieces or i == self.blue_backwards:
                 return True
         #columns
         for i in [0,1,2]:
             column = [row[i] for row in self.board]
-            if column == self.red_pieces or self.red_pieces.reverse():
+            if column == self.red_pieces or column == self.red_backwards:
                 return True
-            if column == self.blue_pieces or self.blue_pieces.reverse():
+            if column == self.blue_pieces or column == self.blue_backwards:
                 return True
 
         #diagonals
@@ -55,11 +57,35 @@ class numz_game:
                           [self.board[i[0]][i[1]]
                            for i in [[2,0], [1,1], [0,2]]]]
         for i in diagonals:
-            if i == self.blue_pieces or self.blue_pieces.reverse():
+            if i == self.blue_pieces or i == self.blue_backwards:
                 return True
-            if i == self.red_pieces or self.red_pieces.reverse():
+            if i == self.red_pieces or i == self.red_backwards:
                 return True
         return False
+    def red_wins(self):
+        #rows
+        for i in self.board:
+            if i == self.red_pieces or i ==self.red_backwards:
+                return 1
+            if i == self.blue_pieces or i == self.blue_backwards:
+                return 0
+        #columns
+        for i in [0,1,2]:
+            column = [row[i] for row in self.board]
+            if column == self.red_pieces or column == self.red_backwards:
+                return 0
+            if column == self.blue_pieces or column == self.blue_backwards:
+                return 1
+
+        #diagonals
+        diagonals =[[self.board[i][i] for i in [0,1,2]],
+                          [self.board[i[0]][i[1]]
+                           for i in [[2,0], [1,1], [0,2]]]]
+        for i in diagonals:
+            if i == self.blue_pieces or i == self.blue_backwards:
+                return 0
+            if i == self.red_pieces or i == self.red_backwards:
+                return 1
 
 
 
@@ -105,9 +131,11 @@ def play_game(agent_1, agent_2):
         move = players[move_number%2](game_board)
         game_board.play_piece(move[0], move[1])
         move_number+=1
-    return move_number%2
+    return game_board.red_wins()
     print(f'player {move_number%2} lost')
     print (game_board.board)
+    return game_board.red_wins()
+
 
 
 def play_n_games(n, agent_0, agent_1):
@@ -128,8 +156,15 @@ def return_game(agent_1, agent_2):
         move_number+=1
     return game_board
 
+def play_n_moves(n, game_board):
+    counter = 0
+    while counter<n:
+        moves = game_board.valid_moves
+    
 
 if __name__ == "__main__":
-    print(play_n_games(1000,losing_agent, winning_agent))
+    print(play_n_games(10,winning_agent, losing_agent))
+    
+    
     
 
